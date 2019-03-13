df561bac285e05e799bc17faf48f8216696ee959
Luciano  Romualdo <lucianopronatec@hotmail.com>
2019-03-13 11:31:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu Hamburguer</title>
+	<meta charset="utf-8">
+	<!--<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">-->
+	<!--<link rel="stylesheet" type="text/css" href="normalize.css">-->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="container">
+
+	<input type="checkbox" id="menu-hamburguer">
+
+	<label for="menu-hamburguer">
+		<div class="row">
+			<div class="menu">
+				<div class="hamburguer">
+					
+				</div>
+				
+			</div>
+
+			<div class="nav">
+				<ul class="nav__ul">
+					<li class="nav__li"><a class="nav__a" href="">Home</a></li>
+					<li class="nav__li"><a class="nav__a" href="">About</a></li>
+					<li class="nav__li"><a class="nav__a" href="">Contact</a></li>
+				</ul>
+			</div>
+			
+		</div>
+	</label>
+		
+
+	</div>
+
+</body>
+</html>
